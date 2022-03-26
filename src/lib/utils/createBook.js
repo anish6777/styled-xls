f57afb04fb883d830305779c100ce3467a56a3d9
Anish Antony <anish6777@yahoo.com>
2022-03-26 14:09:48 +0530

    Base64 encoding change

--- src/lib/utils/createBook.js
+++ src/lib/utils/createBook.js
@@ -1,11 +1,11 @@
 import createsheet from './createsheet';
 import dataURItoBlob from './dataURItoBlob';
+import base64Encode from './base64Encode';
 
 function createBook(title,data,style){
   const book =createsheet(title,data,{defaultStyle:style},null,null,null,true);
-  let bookBuffer = Buffer.from(book);
   let uri = 'data:application/vnd.ms-excel;base64,'
-  let base64String = bookBuffer.toString('base64');
+  let base64String = base64Encode(book);
   const blob = dataURItoBlob(uri+base64String);
   return blob;
 }
--- src/lib/utils/createFromObjectArray.js
+++ src/lib/utils/createFromObjectArray.js
@@ -1,11 +1,11 @@
 import createsheet from './createsheet';
 import dataURItoBlob from './dataURItoBlob';
+import base64Encode from './base64Encode';
 
 function createFromObjectArray(title,data,style,columnConfig,extraHeaders,lastRow){
   const book =createsheet(title,data,style,columnConfig,extraHeaders,lastRow);
-  let bookBuffer = Buffer.from(book);
   let uri = 'data:application/vnd.ms-excel;base64,'
-  let base64String = bookBuffer.toString('base64');
+  let base64String = base64Encode(book);
   const blob = dataURItoBlob(uri+base64String);
   return blob;
   }
