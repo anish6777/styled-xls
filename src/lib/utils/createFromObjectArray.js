1a672cc26097a1d87a5db86ad4abf8a18ab7df47
Anish Antony <anish6777@yahoo.com>
2022-03-26 15:23:14 +0530

    Base64 change

--- src/lib/utils/createFromObjectArray.js
+++ src/lib/utils/createFromObjectArray.js
@@ -1,11 +1,11 @@
 import createsheet from './createsheet';
 import dataURItoBlob from './dataURItoBlob';
-import base64Encode from './base64Encode';
+import base64 from './base64Encode';
 
 function createFromObjectArray(title,data,style,columnConfig,extraHeaders,lastRow){
   const book =createsheet(title,data,style,columnConfig,extraHeaders,lastRow);
   let uri = 'data:application/vnd.ms-excel;base64,'
-  let base64String = base64Encode(book);
+  let base64String = base64.encode(book);
   const blob = dataURItoBlob(uri+base64String);
   return blob;
   }
